10d776f6ee2ea265551a4502a70f6a80e5fdc917
Aditya Raj Singh <166025105+Adityarajsingh2904@users.noreply.github.com>
2025-07-06 23:25:50 +0530

    Add ledger e2e smoke test

--- ledger-service/jest.config.ts
+++ ledger-service/jest.config.ts
@@ -4,7 +4,12 @@ const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/tests'],
-  coveragePathIgnorePatterns: ['<rootDir>/src/client']
+  coveragePathIgnorePatterns: ['<rootDir>/src/client'],
+  coverageThreshold: {
+    global: {
+      statements: 80
+    }
+  }
 }
 
 export default config
